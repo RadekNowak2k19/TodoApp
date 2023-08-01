a67ae075d7e2860728d7e737972ace72c3a3af56
Radosław Nowak <radeknowak2k19@gmail.com>
2023-08-01 09:15:37 +0200

    First commit

---
+++ src/images/index.ts
@@ -0,0 +1,21 @@
+import iconCheck from "./icon-check.svg";
+import iconCross from "./icon-cross.svg";
+import iconMoon from "./icon-moon.svg";
+import iconSun from "./icon-sun.svg";
+import iconDrag from "./iconDrag.svg";
+import bgDesktopDark from "./bg-desktop-dark.jpg";
+import bgDekstopLight from "./bg-desktop-light.jpg";
+import bgMobileDark from "./bg-mobile-dark.jpg";
+import bgMobileLight from "./bg-mobile-light.jpg";
+
+export {
+	iconCheck,
+	iconCross,
+	iconMoon,
+	iconSun,
+	iconDrag,
+	bgDesktopDark,
+	bgDekstopLight,
+	bgMobileDark,
+	bgMobileLight,
+};
